02f1ff3a37b03f0ff81970b4f0232d7559ad01fd
Juricka Tomas <tomas.juricka@outlook.cz>
2018-01-22 09:39:16 +0100

    Basic structure and copied some code.

---
+++ LunchAgent/Helpers/MenuParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using HtmlAgilityPack;
+using LunchAgent.Entities;
+
+namespace LunchAgent.Helpers
+{
+    public class LoaderHelper
+    {
+        private readonly WebClient _webClient;
+        private readonly Logger _logger;
+
+        public LoaderHelper()
+        {
+            _webClient = new WebClient();
+            _logger = new Logger();
+        }
+
+        public List<MenuItem> GetMenuFromMenicka(string url)
+        {
+            var result = new List<MenuItem>();
+
+            var document = new HtmlDocument();
+
+            document.Load(_webClient.OpenRead(url));
+
+            result = ParseMenuFromMenicka(document.DocumentNode);
+
+            return result;
+        }
+
+        private List<MenuItem> ParseMenuFromMenicka(HtmlNode todayMenu)
+        {
+            var result = new List<MenuItem>();
+
+            var foodMenus = todayMenu.SelectNodes(".//tr")
+                .Where(node => node.GetClasses().Contains("soup") || node.GetClasses().Contains("main"));
+
+
+
+            return result;
+        }
+    }
+}
