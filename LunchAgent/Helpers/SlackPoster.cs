a689afcbf8ec494c349bd85f621a37583b426bb5
Juricka Tomas <tomas.juricka@outlook.cz>
2018-03-08 16:13:31 +0100

    Added update switch and logic

--- LunchAgent/Helpers/SlackPoster.cs
+++ LunchAgent/Helpers/SlackPoster.cs
@@ -10,12 +10,15 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace LunchAgent.Helpers
 {
     class SlackPoster
     {
-        private static readonly string Uri = "https://slack.com/api/chat.postMessage";
+        private static readonly string PostMessageUri = "https://slack.com/api/chat.postMessage";
+        private static readonly string UpdateMessageUri = "https://slack.com/api/chat.update";
+        private static readonly string ChatHistoryUri = "https://slack.com/api/channels.history";
 
         public static string PostMenu(List<Tuple<RestaurantSettings, List<MenuItem>>> parsedMenus, string slackFilePath)
         {
@@ -27,7 +30,7 @@ namespace LunchAgent.Helpers
 
             using (var client = new WebClient())
             {
-                var response = client.UploadValues(Uri,"POST", data);
+                var response = client.UploadValues(PostMessageUri,"POST", data);
 
                 result = new UTF8Encoding().GetString(response);
             }
@@ -51,6 +54,67 @@ namespace LunchAgent.Helpers
             return string.Join(Environment.NewLine + Environment.NewLine, result);
         }
 
+        internal static void UpdateMenu(List<Tuple<RestaurantSettings, List<MenuItem>>> menus, string slackFilePath)
+        {
+            var timestamp = GetLastMessageTimestamp(slackFilePath);
+
+            if(string.IsNullOrEmpty(timestamp))
+                return;
+
+            var data = GetDataFromFile(slackFilePath);
+
+            data.Remove("bot_id");
+
+            data["text"] = FormatMenuForSlack(menus);
+            data["ts"] = timestamp;
+
+            using (var client = new WebClient())
+            {
+                var response = client.UploadValues(UpdateMessageUri, "POST", data);
+
+                var result = new UTF8Encoding().GetString(response);
+            }
+        }
+
+        private static string GetLastMessageTimestamp(string slackFilePath)
+        {
+            var data = GetDataFromFile(slackFilePath);
+
+            var timeStamp = DateTime.Today;
+            var result = "";
+
+            using (var client = new WebClient())
+            {
+                var response = client.UploadValues(ChatHistoryUri, "POST", data);
+
+                var stringResponse = new UTF8Encoding().GetString(response);
+
+                dynamic jsonJObject = JObject.Parse(stringResponse);
+
+                var ar = ((JArray)jsonJObject.messages).ToList();
+
+                foreach (dynamic arElement in ar)
+                {
+                    if (arElement.bot_id != data["bot_id"])
+                        continue;
+
+                    string rawTs = arElement.ts;
+
+                    var tsInt = (int) Convert.ToDouble(rawTs.ToString().Replace(".", ","));
+
+                    var tsDate = (new DateTime(1970, 1, 1)).AddSeconds(tsInt);
+
+                    if (tsDate > timeStamp)
+                    {
+                        timeStamp = tsDate;
+                        result = rawTs;
+                    }
+                }
+            }
+
+            return result;
+        }
+
         private static NameValueCollection GetDataFromFile(string filePath)
         {
             var result = new NameValueCollection();
@@ -60,6 +124,7 @@ namespace LunchAgent.Helpers
             {
                 result["token"] = lines[0];
                 result["channel"] = lines[1];
+                result["bot_id"] = lines[2];
             }
             catch (Exception)
             {
@@ -68,6 +133,16 @@ namespace LunchAgent.Helpers
             }
 
             return result;
+        }
+
+        public class SlackMessage
+        {
+            [JsonProperty]
+            public string Text { get; set; }
+            [JsonProperty]
+            public string Type { get; set; }
+            [JsonProperty("ts")]
+            public string TimeStamp { get; set; }
         }
     }
 }
--- LunchAgent/Program.cs
+++ LunchAgent/Program.cs
@@ -14,55 +14,122 @@ namespace LunchAgent
     {
         static void Main(string[] args)
         {
-            ParameterContainer parameters;
-            try
+            var arguments = LoadArguments(args);
+
+            switch (arguments.Option)
             {
-                parameters = LoadArguments(args);
+                case ProgramOption.Help:
+                    PrintHelp();
+                    return;
+                case ProgramOption.Update:
+                case ProgramOption.Post:
+
+                    var restaurantSettingses = JsonParser.ParseFile(arguments.JsonFilePath);
+
+                    var menus = MenuParser.GetMenuFromMenicka(restaurantSettingses);
+
+                    if (CheckMenu(menus) == false)
+                    {
+                        ScheduleUdate();
+                    }
+                    else
+                    {
+                        DeleteUpdate();
+                    }
+
+                    if (arguments.Option == ProgramOption.Post)
+                    {
+                        SlackPoster.PostMenu(menus, arguments.SlackFilePath);
+                    }
+                    else
+                    {
+                        SlackPoster.UpdateMenu(menus, arguments.SlackFilePath);
+                    }
+
+                    break;
             }
-            catch (ApplicationException e)
-            {
-                Console.WriteLine(e);
-                return;
-            }
 
-            var path = parameters.JsonFilePath;
-
-            var parsedJson = JsonParser.ParseFile(path);
-
-            var parsedMenus = MenuParser.GetMenuFromMenicka(parsedJson);
-
-            SlackPoster.PostMenu(parsedMenus, parameters.SlackFilePath);
         }
 
-        public static ParameterContainer LoadArguments(string[] args)
+
+
+        private static bool CheckMenu(List<Tuple<RestaurantSettings, List<MenuItem>>> menus)
         {
-            var result = new ParameterContainer();
+            return !menus.Any(x => x.Item2.Any(y => y.Description == "Pro tento den nebylo zadáno menu."));
+        }
 
-            if(args.Length != 2 )
-                throw new ApplicationException("Invalid number of arguments. Use --help for information about argument usage");
+        private static void ScheduleUdate()
+        {
+            var location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-            var jsonFilePath = args[0];
-            var slackFilePath = args[1];
+            var filename = location + "\\update.txt";
 
-            if(File.Exists(jsonFilePath) == false)
-                throw new ApplicationException($"Could not find json source file: {jsonFilePath}");
+            if (File.Exists(filename) == false)
+            {
+                File.WriteAllLines(filename, new[] { DateTime.Now.ToString() });
+            }
+        }
 
-            if (File.Exists(slackFilePath) == false)
-                throw new ApplicationException($"Could not find json source file: {slackFilePath}");
+        private static void DeleteUpdate()
+        {
+            var location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-            if(Path.GetExtension(jsonFilePath) != ".json")
-                throw new ApplicationException("Invalid extestion of json source file");
+            var filename = location + "\\update.txt";
 
-            result.JsonFilePath = jsonFilePath;
-            result.SlackFilePath = slackFilePath;
+            if (File.Exists(filename) == true)
+            {
+                File.Delete(filename);
+            }
+        }
+
+        public static void PrintHelp()
+        {
+
+        }
+
+        private static ArgumentContainer LoadArguments(string[] args)
+        {
+            var result = new ArgumentContainer();
+
+            if (args.Contains("--help"))
+            {
+                result.Option = ProgramOption.Help;
+                return result;
+            }
+
+            if (args.Length != 3)
+                throw new ArgumentException("Invalid number of arguments");
+
+
+            result.JsonFilePath = args[0];
+            result.SlackFilePath = args[1];
+
+            if (Enum.TryParse(typeof(ProgramOption), args[2], out var parseResult) == false)
+                throw new ArgumentException("Invalid option");
+
+            result.Option = (ProgramOption)parseResult;
+
+            if (File.Exists(result.JsonFilePath) == false)
+                throw new FileNotFoundException("Could not find specified json file");
+
+            if (File.Exists(result.SlackFilePath) == false)
+                throw new FileNotFoundException("Could not find specified slack file");
 
             return result;
         }
 
-        public struct ParameterContainer
+        public struct ArgumentContainer
         {
             public string JsonFilePath { get; set; }
             public string SlackFilePath { get; set; }
+            public ProgramOption Option { get; set; }
+        }
+
+        public enum ProgramOption
+        {
+            Help,
+            Update,
+            Post
         }
     }
 }
